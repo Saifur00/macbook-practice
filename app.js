048c00196393e1b75a7fbf43dc78cd494876150e
Saifur00e <saifurbgc@gmaile.com>
2021-08-21 16:21:08 +0600

    third commit

--- app.js
+++ app.js
@@ -3,72 +3,26 @@ function addCost(upgrade,price){
     
     const memoryCostElement = document.getElementById(upgrade+'-cost');
     memoryCostElement.innerText = price;
-    // const balance = totalCost(upgrade);
-    // const totalCostElement = document.getElementById('total-cost');
-    // totalCostElement.innerText = balance;
-    totalCost();
+    console.log(memoryCostElement.innerText);
+    const memoryCostValue = parseFloat(memoryCostElement.innerText);
+    totalCost(memoryCostValue);
+    
 }
 
-// let sum=0;
 
 
-function totalCost(){
-    // const totalCostElement = document.getElementById('total-cost');
-    // const totalCostText = totalCostElement.innerText;
-    // const totalCostValue = parseFloat(totalCostText);
-    // const totalBalance = totalCostValue+cost;
-    // totalCostElement.innerText = totalBalance;
-    // const currentField = document.getElementById(idField+'-cost');
-    // const currentFieldText = currentField.innerText;
-    // // console.log(currentFieldText);
-    // const currentFieldValue = parseFloat(currentFieldText);
-    // console.log(currentFieldValue);
-    // const totalCostElement = document.getElementById('base-price');
-    // // console.log(totalCostElement);
-    // const totalCostText = totalCostElement.innerText;
-    // const totalCostValue = parseFloat(totalCostText);
-    // console.log(totalCostValue);
-    
-    // const totalBalance= totalCostValue+currentFieldValue;
 
-    const memoryPrice = document.getElementById('memory-cost')
-    console.log(memoryPrice);
-    const storagePrice = document.getElementById('storage-cost');
-    console.log(storagePrice);
-    const deliveryPrice = document.getElementById('delivery-cost');
-    console.log(deliveryPrice);
-    const memoryPriceRightNow = memoryPrice.innerText;
-    console.log(memoryPriceRightNow);
-    const storagePriceRightNow = storagePrice.innerText;
-    console.log(storagePriceRightNow);
-    const deliveryPriceRightNow = deliveryPrice.innerText;
-    console.log(deliveryPriceRightNow);
-
-    // console.log(deliveryPriceRightNow);
-    const pr1 = parseFloat(memoryPriceRightNow);
-    const pr2 = parseFloat(storagePriceRightNow);
-    const pr3 = parseFloat(deliveryPriceRightNow);
-    // console.log(pr1,pr2,pr3)
-
-    const total = pr1+pr2+pr3;
-    console.log(total);
-    const balanceUpdate = document.getElementById('total-cost');
+function totalCost(total){
+    const balanceUpdate = document.getElementById('base-price');
     const balanceText = balanceUpdate.innerText;
     const balanceValue = parseFloat(balanceText);
-    balanceUpdate.innerText = balanceValue+total;
-    // totalBalanceUpdate(total);
-    // const balanceUpdate = document.getElementById('total-cost');
-    // balanceUpdate.innerText = totalBalance;
-    // return totalBalance;
-    
+    const totalBalance = balanceValue+total;
+    const balanceUpdate2 = document.getElementById('total-cost');
+    balanceUpdate2.innerText = totalBalance;
+    return totalBalance; 
 }
 
-// function totalBalanceUpdate(totalBalance){
-//     const balanceUpdate = document.getElementById('total-cost');
-//     const balanceText = balanceUpdate.innerText;
-//     const balanceValue = parseFloat(balanceText);
-//     balanceUpdate.innerText = balanceValue+totalBalance;
-// }
+
 
 document.getElementById('8GB-memory').addEventListener('click',function(){
     // console.log('clicked');
@@ -94,7 +48,7 @@ document.getElementById('512GB-storage').addEventListener('click',function(){
 document.getElementById('1TB-storage').addEventListener('click',function(){
     // console.log('clicked');
     addCost('storage',5000);
-  
+   
     
 })
 document.getElementById('delivery-free').addEventListener('click',function(){
@@ -106,8 +60,10 @@ document.getElementById('delivery-charged').addEventListener('click',function(){
     // console.log('clicked');
     addCost('delivery',20);
   
-
+  
 })
+
+
 
 //  const balanceUpdate = document.getElementById('total-cost');
 //   balanceUpdate.innerText = balance;
